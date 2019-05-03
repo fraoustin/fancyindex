4c5061f0805ba7f9500b65f467aaa0fb8bf1e4f6
Josh Bentley <jbentley@Joshs-MacBook-Pro.local>
2019-05-03 11:58:59 +0100

    fix sed systax to work on linux not mac

--- src/00_init.sh
+++ src/00_init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ "$DISABLE_AUTH" == true ]; then
-    sed -i '' '/auth_basic/s/^/#/' /etc/nginx/conf.d/default.conf
+    sed -i '/auth_basic/s/^/#/' /etc/nginx/conf.d/default.conf
 elif [ ! -z "$WEBUSER" ]; then
     addauth $WEBUSER $WEBPASSWORD
 fi    
